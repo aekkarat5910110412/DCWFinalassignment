f057359c46a96dc3a898774a861f1afa438938b1
เอกรัฐ สุขเขียว <ln_waut@hotmail.com>
2022-03-30 21:33:01 +0700

    edit

--- web-assignfinal/src/App.js
+++ web-assignfinal/src/App.js
@@ -1,30 +1,8 @@
 import FacebookLogin from 'react-facebook-login';
-import axios from 'axios'; 
 
-axios.interceptors.request.use(function(config){
-  const token = sessionStorage.getItem('access_token')
-  if(token)
-   config.headers['Authorization'] = `Bearer ${token}`
-  return config
-}, function(err){
-  return Promise.reject(err)
-})
-const responseFacebook = async (response) => {
-  if(response.accessToken){
-    console.log('log in with accessToken=' + response.accessToken)
-    let result = await axios.post('localhost:8080/api/login',{
-      token: response.accessToken
-    })
-    console.log(result.data);
-    sessionStorage.setItem('access_token', result.data.access_token)
-  }
+const responseFacebook = (response) => {
+  console.log(response);
 }
-
-const callInfoAPI = async () => {
- let result =await axios.get('http://localhost:8080/api/info')
- console.log(result.data)
-}
-
 function App() {
   return (
     <div >
@@ -33,7 +11,7 @@ function App() {
       autoLoad={true}
       fields="name,email,picture"
       callback={responseFacebook} />
-      <button onClick={callInfoAPI}></button>
+     
     </div>
   );
 }
