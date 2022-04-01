70dfac1691ebacc74ef53e61658beffca7f5785c
เอกรัฐ สุขเขียว <ln_waut@hotmail.com>
2022-04-02 04:16:19 +0700

    generate-token

--- Login-Api/main.js
+++ Login-Api/main.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+var jwt = require('jsonwebtoken');
+const keysecret = 'Login'
 
 app.use(cors())
 const mysql = require('mysql2');
@@ -16,6 +18,7 @@ const connection = mysql.createConnection({
   });
 
 app.post('/register', jsonParser, function (req, res, next) {
+  //encrpt password to hash
   bcrypt.hash(req.body.password, saltRounds).then(function(hash) {
      connection.execute(
         'INSERT INTO users (email, password, fname, llname) VALUES (?, ?, ?, ?)',
@@ -29,6 +32,28 @@ app.post('/register', jsonParser, function (req, res, next) {
         }
       );
   });
+})
+
+app.post('/login', jsonParser, function (req, res, next) {
+  connection.execute(
+    'SELECT * FROM users WHERE email=?',
+    [req.body.email],
+    function(err, users, fields) {
+      if (err) {res.json({status:'error',message: err});return}
+      if (users.legth==0){res.json({status:'error',message: 'no user found'});return}
+     //load hash from your password DB
+      bcrypt.compare(req.body.password, users[0].password,function(err,islogin) {
+        if(islogin){
+    //generate token for user login
+          var token = jwt.sign({ email:users[0].email }, keysecret);
+          res.json({status:'ok',message:'login success',token})
+        }
+        else{
+          res.json({status:'error',message:'login failed'})
+        }
+    });     
+    }
+  );
 })
 
 app.listen(3333, function () {
