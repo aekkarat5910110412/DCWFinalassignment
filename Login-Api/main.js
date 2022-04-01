236db518ff45f1688fa013b4431a8241bb8aba2d
เอกรัฐ สุขเขียว <ln_waut@hotmail.com>
2022-04-02 02:28:39 +0700

    mysql-postjson

--- Login-Api/main.js
+++ Login-Api/main.js
@@ -1,11 +1,31 @@
 var express = require('express')
 var cors = require('cors')
 var app = express()
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
 app.use(cors())
+const mysql = require('mysql2');
+//create connec to database mysql
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'mydb'
+  });
 
-app.get('/register', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
+app.post('/register', jsonParser, function (req, res, next) {
+    connection.execute(
+        'INSERT INTO users (email, password, fname, llname) VALUES (?, ?, ?, ?)',
+        [req.body.email, req.body.password, req.body.fname, req.body.llname],
+        function(err, results, fields) {
+          if(err){
+              res.json({status:'error',message: err})
+              return
+          }
+          res.json({status:'ok'})
+        }
+      );
+  
 })
 
 app.listen(3333, function () {
